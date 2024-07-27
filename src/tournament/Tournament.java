ac1dfe1854cd4ae579a3830aedfde68dd63d89e2
12227642 <e12227642@student.tuwien.ac.at>
2024-07-27 12:53:42 +0200

    realigned level 3 & 4

--- src/tournament/Tournament.java
+++ src/tournament/Tournament.java
@@ -3,7 +3,6 @@ package tournament;
 import fightingStyles.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,22 +27,28 @@ public class Tournament {
 
     public Tournament(int rocks, int papers, int scissors, FightingStyle winningStyle, int safeRound){
         //attributing winning, neutral and prohibited Style and amount
-        FightingStyle prohibitedStyle = winningStyle.getsBeatenBy();
-        FightingStyle neutralStyle = prohibitedStyle.getsBeatenBy();
+        FightingStyle prohibitedStyle;
+        FightingStyle neutralStyle;
         HashMap<FightingStyle, Integer> amountMap = new HashMap<>();
 
         switch (winningStyle) {
             case Paper paper -> {
+                prohibitedStyle = Scissors.getInstance();
+                neutralStyle = Rock.getInstance();
                 amountMap.put(winningStyle, papers);
                 amountMap.put(prohibitedStyle, scissors);
                 amountMap.put(neutralStyle, rocks);
             }
             case Rock rock -> {
+                prohibitedStyle = Paper.getInstance();
+                neutralStyle = Scissors.getInstance();
                 amountMap.put(winningStyle, rocks);
                 amountMap.put(prohibitedStyle, papers);
                 amountMap.put(neutralStyle, scissors);
             }
             case Scissors scissor -> {
+                prohibitedStyle = Rock.getInstance();
+                neutralStyle = Paper.getInstance();
                 amountMap.put(winningStyle, scissors);
                 amountMap.put(prohibitedStyle, rocks);
                 amountMap.put(neutralStyle, papers);
@@ -54,30 +59,27 @@ public class Tournament {
         //creating participant list iteratively
         List<FightingStyle> participants = new ArrayList<>();
         //first, get all prohibited ones out of the way while neutralising them
-        while (amountMap.get(prohibitedStyle) >= 3) {
-            participants.add(prohibitedStyle);
-            participants.add(prohibitedStyle);
-            participants.add(prohibitedStyle);
+        while (amountMap.get(prohibitedStyle) >= Math.pow(2, safeRound + 1) - 1) {
+            for (int i = 0; i < Math.pow(2, safeRound + 1) - 1; i++) {
+                participants.add(prohibitedStyle);
+            }
+            amountMap.replace(prohibitedStyle, amountMap.get(prohibitedStyle) - (int) Math.pow(2, safeRound + 1) - 1);
             participants.add(neutralStyle);
             amountMap.replace(neutralStyle, amountMap.get(neutralStyle) - 1);
         }
         //clear edge case (2 prohibited, 1 neutral)
-        if(amountMap.get(prohibitedStyle) == 2 && amountMap.get(neutralStyle) == 1){
-            participants.add(prohibitedStyle);
+        if(amountMap.get(neutralStyle) >= 1){
             participants.add(neutralStyle);
-            participants.add(prohibitedStyle);
+            amountMap.replace(neutralStyle, amountMap.get(neutralStyle) - 1);
             //if the edge case happens, in order to be a valid input, there must be at least 5 winning type remaining
-        } else {
-            //clean up the remaining ones
-            for (int i = 0; i < amountMap.get(prohibitedStyle); i++) {
-                participants.add(prohibitedStyle);
-            }
-            //neutralise remaining
-            for (int i = 0; i < amountMap.get(neutralStyle); i++) {
-                participants.add(neutralStyle);
-            }
         }
-        //secure winner
+        //clean up the remaining ones
+        for (int i = 0; i < amountMap.get(prohibitedStyle); i++) {
+            participants.add(prohibitedStyle);
+        }
+        for (int i = 0; i < amountMap.get(neutralStyle); i++) {
+            participants.add(neutralStyle);
+        }
         for (int i = 0; i < amountMap.get(winningStyle); i++) {
             participants.add(winningStyle);
         }
@@ -202,7 +204,7 @@ public class Tournament {
     }
 
     public List<FightingStyle> buildWinner(List<FightingStyle> participants, FightingStyle winningStyle) {
-
+        //base case, if the input already produces the winningStyle (or better as defined by the finalWinner comparator), then it will just be returned
         if (Scissors.getInstance().getComparator().compare(winningStyle, new Tournament(FightingStyle.getCharsFromList(participants)).root.calcParticipant()) <= 0) {
             return participants;
         }
@@ -210,33 +212,30 @@ public class Tournament {
         //difficult case
         List<FightingStyle> lList = new ArrayList<>();
 
-        //sort the List
+        //sort the List by the comparator
         participants.sort(Scissors.getInstance().getComparator());
 
-
-        FightingStyle toBeAdded = participants.getFirst();
         FightingStyle leftWinner;
 
         //split the List
         if (participants.stream().anyMatch(fightingStyle -> fightingStyle.equals(winningStyle.getDoubleBeats()))) {
+            //adding one neutralises the whole half
             leftWinner = winningStyle.getDoubleBeats();
             lList.add(leftWinner);
             participants.remove(leftWinner);
 
-            while (participants.getFirst().equals(toBeAdded) && lList.size() < participants.size()){
-                lList.add(participants.removeFirst());
-            }
         } else {
             leftWinner = winningStyle.getBeats();
             lList.add(leftWinner);
             participants.remove(leftWinner);
 
             if (participants.stream().anyMatch(f -> f.equals(leftWinner.getsDoubleBeatenBy()))) {
-                //then there needs to be max this amount to neutralise else just fill with
+                //if there is at least one which can beat winning style, then ensure validity by adding enough intermediate neutralizers
                 for (int i = 0; i < Math.log(participants.size() / 2.0) / Math.log(2); i++) {
                     if(participants.remove(leftWinner.getDoubleBeats())) {
                         lList.add(leftWinner.getDoubleBeats());
                     } else {
+                        //if there are less intermediate neutralizers, there also have to be less winningStyle beaters
                         break;
                     }
                 }
